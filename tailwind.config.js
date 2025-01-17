05c83a07c7466b782401cf3bd0da5c0839306664
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-01-17 13:20:46 +0200

    some sections added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'bgray' :'#d5d5d5'
+        'bgray' :'#d5d5d5',
+        'headertext' :'#a18a4d',
+        'ptext': '#212529'
       }
     },
   },
